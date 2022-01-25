4227aef5a6fb9ef8764b4becf9366c237a84eb6a
hiba-machfej <58190902+hiba-machfej@users.noreply.github.com>
2022-01-25 15:37:21 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './containers/Home';
+import About from './containers/About';
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
